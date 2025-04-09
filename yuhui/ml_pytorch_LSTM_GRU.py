b7c4fed56f95d7a9a955021bb98b798c833e016c
jibril-b-coulibaly <jibril.coulibaly@gmail.com>
2025-04-09 16:52:45 -0600

    make functions from the Jupyter blocks of code that extract and normalize inputs and output

--- yuhui/ml_pytorch_LSTM_GRU.py
+++ yuhui/ml_pytorch_LSTM_GRU.py
@@ -84,113 +84,127 @@ print(R)
 
 
 # In[ ]:
-
-
-import numpy as np
-
-
-sequence_length = 1000  # 每个序列的时间步
-input_n_features = 6
-output_n_features = 6
-
-input_columns = ["strain11", "strain22", "strain33", "strain12", "strain13", "strain23"]
-output_columns = ["stress11", "stress22", "stress33", "stress12", "stress13", "stress23"]
-
-# 重新计算符合条件的样本数
-valid_indices = df_combined['index'].unique()
-count = len(valid_indices)   # 每个 index 有 3 组数据
-
-# 初始化 X 和 y 数组
-X = np.zeros((count, sequence_length, input_n_features))
-y = np.zeros((count, sequence_length, output_n_features))
-
-# 填充 X 和 y
-count = 0
-for i in valid_indices:
-    df1 = df_combined[df_combined['index'] == i]
-    df1 = df1.sort_values(by="step")  # 确保 step 顺序正确
     
-    # 按 step 递增分成三组，每组取 1000 行
-    for j in range(3):
-        subset = df1.iloc[j * sequence_length : (j + 1) * sequence_length]
-        if len(subset) == sequence_length:
-            X[count] = subset[input_columns].to_numpy()
-            y[count] = subset[output_columns].to_numpy()
-            count += 1
+# TODO JBC: Looks like what was in this Jupyter block and the next one could be
+# manually chosen by the user to use either / or techniques to extract X and y
+# from the data. To keep them and have them work in a regular script,
+# I refactored them into functions
+def extract_input_and_output(df_combined,
+                             sequence_length = 1000,
+                             input_n_features = 6,
+                             output_n_features = 6,
+                             input_columns = ["strain11", "strain22", "strain33", "strain12", "strain13", "strain23"],
+                             output_columns = ["stress11", "stress22", "stress33", "stress12", "stress13", "stress23"]):
+    # 重新计算符合条件的样本数
+    valid_indices = df_combined['index'].unique()
+    count = len(valid_indices)   # 每个 index 有 3 组数据
 
-# 输出最终 X, y 的形状
+    X = np.zeros((count, sequence_length, input_n_features))
+    y = np.zeros((count, sequence_length, output_n_features))
+
+    count = 0
+    for i in valid_indices:
+        df1 = df_combined[df_combined['index'] == i]
+        df1 = df1.sort_values(by="step") # In case steps are not ordered (TODO: can be disordered by previous line? or always in order and this unnecessary?)
+
+        # 按 step 递增分成三组，每组取 1000 行
+        # TODO: I don't understand what is happening here
+        # Unless there can be more than 1000 steps for a given index, df1 should always be size 1000
+        # and the code below is pointless as j=0 will always return. What am I missing?
+        for j in range(3):
+            subset = df1.iloc[j * sequence_length : (j + 1) * sequence_length]
+            if len(subset) == sequence_length:
+                X[count] = subset[input_columns].to_numpy()
+                y[count] = subset[output_columns].to_numpy()
+                count += 1
+    return X, y
+
+X, y = extract_input_and_output(df_combined)
 print(X.shape, y.shape)
 
+# TODO JBC: Looks like what was in this Jupyter block and the previous one could be
+# manually chosen by the user to use either / or techniques to extract X and y
+# from the data. To keep them and have them work in a regular script,
+# I refactored them into functions
+def extract_and_normalize_input_and_output(df_combined,
+                                           sequence_length = 1000,
+                                           input_n_features = 6,
+                                           output_n_features = 6,
+                                           input_columns = ["strain11", "strain22", "strain33", "strain12", "strain13", "strain23"],
+                                           output_columns = ["stress11", "stress22", "stress33", "stress12", "stress13", "stress23"]):
+    # 重新计算符合条件的样本数
+    valid_indices = df_combined['index'].unique()
+    count = len(valid_indices)   # 每个 index 有 3 组数据
 
-# In[ ]:
+    X = np.zeros((count, sequence_length, input_n_features))
+    y = np.zeros((count, sequence_length, output_n_features))
 
+    count = 0
+    for i in valid_indices:
+        df1 = df_combined[df_combined['index'] == i]
+        df1 = df1.sort_values(by="step") # In case steps are not ordered (TODO: can be disordered by previous line? or always in order and this unnecessary?)
 
-# 重新计算符合条件的样本数
-count = len(valid_indices)   # 每个 index 有 3 组数据
+        # 按 step 递增分成三组，每组取 1000 行
+        # TODO: I don't understand what is happening here
+        # Unless there can be more than 1000 steps for a given index, df1 should always be size 1000
+        # and the code below is pointless as j=0 will always return. What am I missing?
+        for j in range(3):
+            subset = df1.iloc[j * sequence_length : (j + 1) * sequence_length]
+            if len(subset) == sequence_length:
+                # Standardize inputs to unit variance
+                # TODO: is the mean of the input zero ? If not, doing this changes the mean !
+                std_devs_in = subset[input_columns].std().to_numpy()
+                std_devs_out = subset[output_columns].std().to_numpy()
+                # Avoid division by zero
+                std_devs_in[std_devs_in <= 0] = 1e-6
+                std_devs_out[std_devs_out <= 0] = 1e-6
+                X[count] = subset[input_columns].to_numpy() / std_devs_in
+                y[count] = subset[output_columns].to_numpy() / std_devs_out
 
-# 初始化 X 和 y 数组
-X = np.zeros((count, sequence_length, input_n_features))
-y = np.zeros((count, sequence_length, output_n_features))
+                # TODO: DO NOT SHARE UNDOCUMENTED COMMENTED CODE !!!!!!!!!!!!!
+                # TODO: SHOULD THIS BE USED? WHEN? HOW?
+                # NO ONE KNOWS! AND THOSE WHO DO WILL EVENTUALLY FORGET OR CHANGE JOB !
 
-# 填充 X 和 y
-count = 0
-for i in valid_indices:
-    df1 = df_combined[df_combined['index'] == i]
-    df1 = df1.sort_values(by="step")  # 确保 step 顺序正确
+                        #  # Compute normalization parameters
+                        #  X_min = subset[input_columns].min().to_numpy()  # Minimum values for each feature
+                        #  X_max = subset[input_columns].max().to_numpy()  # Maximum values for each feature
 
-    # 按 step 递增分成三组，每组取 1000 行
-    for j in range(3):
-        subset = df1.iloc[j * sequence_length : (j + 1) * sequence_length]
-        if len(subset) == sequence_length:
-            # 计算标准差
-            std_devs_in = subset[input_columns].std().to_numpy()
-            std_devs_out = subset[output_columns].std().to_numpy()
+                        #  X_m = (X_min + X_max) / 2  # Mean of min and max
+                        #  X_s = (X_max - X_min) / 2  # Scaling factor
 
-            # 避免除零错误
-            std_devs_in[std_devs_in == 0] = 1e-6
-            std_devs_out[std_devs_out == 0] = 1e-6
+                        # # Normalize X using the given formula
+                        #  X[count] = (subset[input_columns].to_numpy() - X_m) / X_s
 
-            #归一化数据
-            X[count] = subset[input_columns].to_numpy() / std_devs_in
-            y[count] = subset[output_columns].to_numpy() / std_devs_out
-           #  # Compute normalization parameters
-           #  X_min = subset[input_columns].min().to_numpy()  # Minimum values for each feature
-           #  X_max = subset[input_columns].max().to_numpy()  # Maximum values for each feature
+                        # # Compute normalization parameters for y
+                        #  y_min = subset[output_columns].min().to_numpy()
+                        #  y_max = subset[output_columns].max().to_numpy()
 
-           #  X_m = (X_min + X_max) / 2  # Mean of min and max
-           #  X_s = (X_max - X_min) / 2  # Scaling factor
+                        #  y_m = (y_min + y_max) / 2
+                        #  y_s = (y_max - y_min) / 2
 
-           # # Normalize X using the given formula
-           #  X[count] = (subset[input_columns].to_numpy() - X_m) / X_s
+                        #  # Normalize y using the given formula
+                        #  y[count] = (subset[output_columns].to_numpy() - y_m) / y_s
+                        #  # X[count] = subset[input_columns].to_numpy()
+                        #  # y[count] = subset[output_columns].to_numpy()
 
-           # # Compute normalization parameters for y
-           #  y_min = subset[output_columns].min().to_numpy()
-           #  y_max = subset[output_columns].max().to_numpy()
+                # Normalize input data in range [0, 1]
+                # TODO: Normalizing between [0, 1] changes the variance again
+                # so the standardization we did prior might be better done at the end
+                # and normalization with a zero mean, e.g., between [-1, 1] might be preferred
+                X_min = subset[input_columns].min().to_numpy()
+                X_max = subset[input_columns].max().to_numpy()
+                X[count] = (subset[input_columns].to_numpy() - X_min) / (X_max - X_min)
+                # Normalize output data in range [0, 1]
+                # TODO: Normalizing between [0, 1] changes the variance again
+                # so the standardization we did prior might be better done at the end
+                # and normalization with a zero mean, e.g., between [-1, 1] might be preferred
+                y_min = subset[output_columns].min().to_numpy()
+                y_max = subset[output_columns].max().to_numpy()
+                y[count] = (subset[output_columns].to_numpy() - y_min) / (y_max - y_min)
+                count += 1
+    return X, y
 
-           #  y_m = (y_min + y_max) / 2
-           #  y_s = (y_max - y_min) / 2
-
-           #  # Normalize y using the given formula
-           #  y[count] = (subset[output_columns].to_numpy() - y_m) / y_s
-           #  # X[count] = subset[input_columns].to_numpy() 
-           #  # y[count] = subset[output_columns].to_numpy() 
-            # Compute normalization parameters
-            X_min = subset[input_columns].min().to_numpy()  # Minimum values for each feature
-            X_max = subset[input_columns].max().to_numpy()  # Maximum values for each feature
-
-          # Normalize X using min-max normalization
-            X[count] = (subset[input_columns].to_numpy() - X_min) / (X_max - X_min)
-
-         # Compute normalization parameters for y
-            y_min = subset[output_columns].min().to_numpy()
-            y_max = subset[output_columns].max().to_numpy()
-
-          # Normalize y using min-max normalization
-            y[count] = (subset[output_columns].to_numpy() - y_min) / (y_max - y_min)
-
-            count += 1
-
-# 输出最终 X, y 的形状
+X, y = extract_and_normalize_input_and_output(df_combined)
 X.shape, y.shape
 
 
